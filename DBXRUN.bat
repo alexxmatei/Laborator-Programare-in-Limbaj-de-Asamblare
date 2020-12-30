cc2c767c8273b021380585a566d17fa5a55d2ef4
Al <alexxmatei@gmail.com>
2020-12-30 13:01:56 +0200

    Added a more detailed description for the DBX scripts.

--- DBXRUN.bat
+++ DBXRUN.bat
@@ -1,4 +1,10 @@
-:: starts DOSBOX with the following commands:
-:: -c  "cd %2" change directory to the second parameter given
-:: -c "dbg %1" run the DBG.bat file on the file with the name given from the first parameter
-dosbox start -c "cd %2" -c "dbg %1"
+:: Starts DOSBOX with the following commands:
+::   -c  "CD %2" change directory to the second parameter given
+::   -c "RUN %1" run the RUN.bat script on the file with the name given as the first parameter
+:: Function call example:
+::   starts DOSBOX, changes directory (CD) to luc01 and then executes the RUN command on the l1a1 file
+::     DBXDBG.bat l1a1 luc01  
+::   starts DOSBOX, stays in root folder (no CD directory provided) and then executes the RUN command on the add2 file
+::     DBXDBG.bat add2
+
+dosbox start -c "CD %2" -c "RUN %1"
