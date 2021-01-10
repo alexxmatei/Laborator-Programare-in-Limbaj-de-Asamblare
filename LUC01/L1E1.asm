67a1e3ee545292e8b771c413591b36f7c12b4971
Al <alexxmatei@gmail.com>
2021-01-10 16:33:02 +0200

    Updated formatting.

--- LUC01/L1E1.asm
+++ LUC01/L1E1.asm
@@ -13,36 +13,36 @@
       mov ax, @data
       mov ds, ax
 
-    mov ax, 14h               ; adresare imediata
-    mov ax, 14                ; adresare imediata
-    mov al, tabel             ; adresare directa
-    mov al, tabel[1]          ; adresare directa
-    
-    ; operatorul ptr este necesar
-    ; tabel fiind definit cu directiva DB
-    mov ax, word ptr tabel    ; adresare directa
-    mov ax, word ptr tabel[2] ; adresare directa
-    
-    mov bx, offset tabel
-    mov al, [bx + 1]         ; adresare indirecta
-    mov al, [bx]             ; adresare indirecta
-    mov bx, 5                ; adresare imediata
-    mov al, tabel[bx]        ; adresare bazata
-    mov si, 1                ; adresare imediata
-    mov al, [bx][si]         ; adresare bazata si indexata
-    mov si, 6                ; adresare imediata
+      mov ax, 14h               ; adresare imediata
+      mov ax, 14                ; adresare imediata
+      mov al, tabel             ; adresare directa
+      mov al, tabel[1]          ; adresare directa
+      
+      ; operatorul ptr este necesar
+      ; tabel fiind definit cu directiva DB
+      mov ax, word ptr tabel    ; adresare directa
+      mov ax, word ptr tabel[2] ; adresare directa
+      
+      mov bx, offset tabel
+      mov al, [bx + 1]         ; adresare indirecta
+      mov al, [bx]             ; adresare indirecta
+      mov bx, 5                ; adresare imediata
+      mov al, tabel[bx]        ; adresare bazata
+      mov si, 1                ; adresare imediata
+      mov al, [bx][si]         ; adresare bazata si indexata
+      mov si, 6                ; adresare imediata
 
-    ; explicitarea cu operatorul ptr este absolut necesara
-    mov byte ptr [bx][si], 2 ; adresare bazata si indexata
+      ; explicitarea cu operatorul ptr este absolut necesara
+      mov byte ptr [bx][si], 2 ; adresare bazata si indexata
 
-    mov bp, offset tabel
-    mov al, [bp]      ; adresare bazata cu deplasament nul
+      mov bp, offset tabel
+      mov al, [bp]      ; adresare bazata cu deplasament nul
 
-    ; explicitarea cu operatorul ptr este absolut necesara
-    ; adresare bazata si indexata
-    mov byte ptr ds:[bp][si][1], 7   
-    ; adresare bazata si indexata
-    mov word ptr ds:[bp][si][1], 19h
+      ; explicitarea cu operatorul ptr este absolut necesara
+      ; adresare bazata si indexata
+      mov byte ptr ds:[bp][si][1], 7   
+      ; adresare bazata si indexata
+      mov word ptr ds:[bp][si][1], 19h
 
     ; iesire din program
     sfarsit:
