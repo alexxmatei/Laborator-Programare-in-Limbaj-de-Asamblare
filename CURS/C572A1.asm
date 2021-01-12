3484db4eb80de3bab3f0847ae72c13c3093f7ffa
Al <alexxmatei@gmail.com>
2021-01-12 08:43:24 +0200

    First program attempt draft.

---
+++ CURS/C572A1.asm
@@ -0,0 +1,60 @@
+linie_noua MACRO
+    mov dl, 13 ; Carriage Return
+    mov ah, 2
+    int 21h
+    mov dl, 10 ; Line Feed
+    mov ah, 2
+    int 21h
+ENDM
+
+putsi macro x
+  local sir
+    .data
+      sir db x, 0
+    .code
+        push si
+        lea si, sir
+        call afis_sir
+        pop si
+endm
+
+.model small
+.stack 100 ; dimensiunea stivei
+.code
+afis_sir proc far
+    push ax
+    push si
+    push dx
+  iar:
+    test dl, [si]
+    test dl, dl
+    jz ultim
+    mov ah, 2h
+    int 21h
+    inc si
+    jmp iar
+  ultim:
+    pop dx
+    pop si
+    pop ax
+    ret
+afis_sir endp
+
+putsi 'topkek'
+
+;program:
+  ; muta adresa segmentului de date in DS
+  initializare:
+    mov ax, @data
+    mov ds, ax
+
+  start:
+    ;instructiuni program
+
+  ; iesire din program
+  sfarsit:
+    ; adauga un caracter de tip 'newline', linie noua
+    linie_noua
+    mov ah, 4Ch    ; functia DOS de iesire din program
+    int 21h
+end ;program
