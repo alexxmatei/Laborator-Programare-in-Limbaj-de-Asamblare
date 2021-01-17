c2b7217982134b073678894f93ea13a027b02a7a
Al <alexxmatei@gmail.com>
2021-01-17 16:50:37 +0200

    Added C equivalent example for exercise.

---
+++ CURS/apelc_exC.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  unsigned char u_char = 5u;
+  int varUtilizator = 0;
+  char * string = "Un sir";
+  scanf("%d", varUtilizator);
+  printf("%s %u", string, u_char);
+}
